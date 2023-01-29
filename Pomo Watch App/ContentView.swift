07741d1b5b2e26d2725041277b5db5e0920dff61
konan080211 <konanforbis@gmail.com>
2023-01-29 16:53:03 +0900

    fix: 大枠のUIの作成

--- Pomo Watch App/ContentView.swift
+++ Pomo Watch App/ContentView.swift
@@ -2,14 +2,22 @@
 //  ContentView.swift
 //  Pomo Watch App
 //
-//  Created by 小南佑介 on 2023/01/15.
+//  Created by 小南佑介 on 2023/01/05
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ProgressCircle(limit: 1500, progress: 1255)
+        VStack{
+            Spacer()
+            ProgressCircle(limit: 1500, progress: 1255)
+            HStack{
+                RunningTask()
+                Spacer()
+                OperateTask()
+            }
+        }
     }
 }
 
@@ -24,18 +32,35 @@ struct ProgressCircle: View {
         ZStack{
             Circle()
                 .trim(from: 0, to: value)
-                .stroke(style: StrokeStyle(lineWidth: 19, lineCap: .round, lineJoin: .round))
+                .stroke(style: StrokeStyle(lineWidth: 18, lineCap: .round, lineJoin: .round))
                 .foregroundColor(Color.green)
                 .rotationEffect(Angle(degrees: -90))
-                .frame(width: 157, height: 157)
+                .frame(width: 132, height: 132)
             Text(String(format: "%02d:%02d", min, sec))
                 .font(.largeTitle)
-                .fontWeight(.semibold)
+                .fontWeight(.medium)
                 //.foregroundColor(Color.green)
         }
     }
 }
 
+struct RunningTask: View {
+    var body: some View {
+        Text("MTG")
+            .onTapGesture {
+                print("MTG中です")
+            }
+    }
+}
+
+struct OperateTask: View {
+    var body: some View {
+        Text("•••")
+            .onTapGesture {
+                print("動いてますか")
+            }
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
